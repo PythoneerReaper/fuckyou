85237e13dd51e5b7d0e80e3b95f91896903b9f59
PythoneerReaper <76683569+PythoneerReaper@users.noreply.github.com>
2021-01-20 21:09:23 +0300

    Fuck you too

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 for x in range(900):
-	print("I've come to make an announcement: Github's a bitchass motherfucker. They deleted my fucking account. That’s right, they took their patched fucking repository dick out and they terminated my fucking account, and they said their Terms-of-Service was THIS BIG. And I said “that’s disgusting!” So I’m making a callout post on my twitter dot com. Github, you got a small repository, it’s the size of this Python file except WAY smaller. And guess what, here’s what my repository looks like: (blowing up noises) That’s right baby. All hard-coded, no errors, no flags, look at that it looks like two .py files and a .bat file. They fucked my account so guess what, I’m gonna FUCK GITHUB DOT COM. THATS RIGHT THIS IS WHAT YOU GET, MY REPOSITORY LAZER PISS! Except I’m not gonna piss on Github, I’m gonna go higher. I’m pissing oN THE TERMS OF SEEEERVVIIIICEE!!! HOW DO YOU LIKE THAT, GITHUB SUPPORT?! I PISSED ON THE TERMS OF SERVICE, YOU IDIOTS!!!")
-	print("You have 23 hours before the piss drrrrroplllllets hit Github fucking dot com. Now get out of my fucking sight before I piss on you too.")
+	print("OOPSIE WOOPSIE!! Uwu We make a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!")
+
+	
